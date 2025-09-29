fef49ea5162ca496c87ca90b006184dc821ede2c
Sarah Forde <you@example.com>
2025-09-29 17:01:16 +0100

    Step 3

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import React, {useState} from 'react';
+import Logo from './components/Logo'
 
-export default function AssetExample() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Local files and assets can be imported by dragging and dropping them into the editor
-      </Text>
-      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
-    </View>
-  );
+
+
+export default function App() {
+  const [fullname, setFullname]= useState("Sarah");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
+
+  function buttonClicked() {
+    alert("Hello"+ fname + lname+ ". Your Date of birth is" + dob); 
 }
+  return (
+    <View>
+      <Logo/>
+    
+       <Text>Hello {fname} {lname}. You were born on {dob}</Text>
+       <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+       <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+       <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
+       <Button title="SUBMIT" onPress={buttonClicked}/>
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 24,
-  },
-  paragraph: {
-    margin: 24,
-    marginTop: 0,
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  logo: {
-    height: 128,
-    width: 128,
-  }
-});
+    </View>
+  )
+};
+
+
+
