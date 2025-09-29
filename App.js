236a71d226273ab3c421b79d54f67f6dcfa327f1
Sarah Forde <you@example.com>
2025-09-29 16:10:12 +0100

    Step 1 Logo

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import {View, Text, TextInput} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, {useState} from 'react';
+import Logo from './components/Logo'
 
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
 
-export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
-  return(
-    <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Logo/>
     </View>
-  )
-};
+  );
+}
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
